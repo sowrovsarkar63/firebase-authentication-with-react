192eebdd7e2aae7f98fc61c5c8204ce810c8e7a8
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-06 01:45:22 +0600

    login with google

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import "./App.css";
+import initializeAuthentication from "./Firebase/firebase.initialize";
 
 function App() {
+    initializeAuthentication();
+    const provider = new GoogleAuthProvider();
+    const handleClick = () => {
+        const auth = getAuth();
+        signInWithPopup(auth, provider).then((result) => {
+            // The signed-in user info.
+            const user = result.user;
+            console.log(user);
+            // ...
+        });
+    };
+
     return (
         <div className="App">
             <h1>Learn firebase for first time </h1>
+            <button onClick={handleClick}>Sign In With Google </button>
         </div>
     );
 }
