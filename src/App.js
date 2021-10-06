12ed1f5c20c7dfb5095b9474e305b3906dcdf04f
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-06 20:39:47 +0600

    display user data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from "react";
 import "./App.css";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 
 function App() {
+    const [loggedInuser, setloggedInuser] = useState({});
+
     initializeAuthentication();
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
     const handleClick = () => {
         const auth = getAuth();
-        signInWithPopup(auth, provider).then((result) => {
-            // The signed-in user info.
-            const user = result.user;
-            console.log(user);
-            // ...
-        });
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                // The signed-in user info.
+                const LoginUser = result.user;
+                setloggedInuser(LoginUser);
+                console.log(loggedInuser);
+                // ...
+            })
+            .catch((err) => {
+                console.log("sorry!!");
+            });
     };
-
+    // Github login hadnler
+    /*     const githubProvider = new GithubAuthProvider();
+    const loginGithub = () => {
+        const auth = getAuth();
+        signInWithPopup(auth, githubProvider)
+            .then((result) => {
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                console.log(error);
+                // ...
+            });
+    }; */
     return (
         <div className="App">
             <h1>Learn firebase for first time </h1>
             <button onClick={handleClick}>Sign In With Google </button>
+
+            {/* <button onClick={}>Sign In With Github</button> */}
+            <div>
+                <h2>name is {loggedInuser.displayName}</h2>
+            </div>
         </div>
     );
 }
