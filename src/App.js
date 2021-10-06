952428d4f92f0ef715b7abc8ee7e72948e08eb03
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-07 00:54:40 +0600

    Implement github login with firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+    getAuth,
+    GithubAuthProvider,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from "firebase/auth";
 import { useState } from "react";
 import "./App.css";
 import initializeAuthentication from "./Firebase/firebase.initialize";
@@ -8,8 +13,8 @@ function App() {
 
     initializeAuthentication();
     const googleProvider = new GoogleAuthProvider();
+    const auth = getAuth();
     const handleClick = () => {
-        const auth = getAuth();
         signInWithPopup(auth, googleProvider)
             .then((result) => {
                 // The signed-in user info.
@@ -24,32 +29,37 @@ function App() {
                 // ...
             })
             .catch((err) => {
-                console.log("sorry!!");
+                console.log(err.message);
             });
     };
     // Github login hadnler
-    /*     const githubProvider = new GithubAuthProvider();
+    const githubProvider = new GithubAuthProvider();
     const loginGithub = () => {
-        const auth = getAuth();
         signInWithPopup(auth, githubProvider)
             .then((result) => {
                 // The signed-in user info.
-                const user = result.user;
-                console.log(user);
+                const { displayName, photoURL, email } = result.user;
+                const LoginUser = {
+                    name: displayName,
+                    email: email,
+                    photo: photoURL,
+                };
+                setloggedInuser(LoginUser);
                 // ...
             })
             .catch((error) => {
                 // Handle Errors here.
-                console.log(error);
+                console.log(error.message);
                 // ...
             });
-    }; */
+    };
     return (
         <div className="App">
             <h1>Learn firebase for first time </h1>
             <button onClick={handleClick}>Sign In With Google </button>
+            <button onClick={loginGithub}>Sign In With Github </button>
             <br />
-            {loggedInuser.email ? (
+            {loggedInuser.name ? (
                 <div>
                     <h2>Welocme To {loggedInuser.name}</h2>
                     <h3>Email: {loggedInuser.email}</h3>
