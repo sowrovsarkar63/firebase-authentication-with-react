6d03c72be01def5eb2a8761a5b5ba5c71f952275
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2021-10-07 01:54:54 +0600

    Update firebase.config.js

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCyyHhyJldbareKJI-EZm2h_qt64WVpDQ8",
-    authDomain: "learning-firebase-adde1.firebaseapp.com",
-    projectId: "learning-firebase-adde1",
-    storageBucket: "learning-firebase-adde1.appspot.com",
-    messagingSenderId: "618988298072",
-    appId: "1:618988298072:web:843d7ef4fdbd25ec0e4f2d",
+ 
 };
 
 export default firebaseConfig;
